844b6952bf8c9669d4b338ad55e2505b8e22ab26
John Munro <goodfellow_munro@hotmail.com>
2019-10-10 23:37:14 +0100

    Stage 1: setup .Net Framework 4.5.2 MVC projects
    Customise for GitHub Search website

---
+++ GitHubSearch/GitHubSearch.Tests/Controllers/SearchControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitHubSearch;
+using GitHubSearch.Controllers;
+
+namespace GitHubSearch.Tests.Controllers
+{
+    [TestClass]
+    public class SearchControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            var controller = new SearchController();
+
+            // Act
+            var result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
